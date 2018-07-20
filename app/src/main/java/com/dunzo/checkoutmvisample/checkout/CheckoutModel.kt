b7ae6136ddc04e80edda403ff1f6f7460429083d
Viswanath Kumar Sandu <viswanath@dunzo.in>
2018-07-20 20:41:04 +0530

    CheckoutModel, extract functions.

--- app/src/main/java/com/dunzo/checkoutmvisample/checkout/CheckoutModel.kt
+++ app/src/main/java/com/dunzo/checkoutmvisample/checkout/CheckoutModel.kt
@@ -6,10 +6,15 @@ import io.reactivex.rxkotlin.withLatestFrom
 
 object CheckoutModel {
     fun bind(bindings: Observable<Binding>, cart: Cart): Observable<CheckoutModelState> {
-        val createdUseCaseStates = bindings
+        return bindingsUseCase(bindings, cart)
+    }
+
+    private fun bindingsUseCase(
+            bindings: Observable<Binding>,
+            cart: Cart
+    ): Observable<CheckoutModelState> {
+        return bindings
                 .withLatestFrom(cart.summaries()) { _, cartSummary -> cartSummary }
                 .map { cartSummary -> CheckoutModelState(cart.getCartItems(), cartSummary) }
-
-        return createdUseCaseStates
     }
 }
